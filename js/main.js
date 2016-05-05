f2a1aa57aceff943ee44aa7dc85009979731355d
Yauhen <Arucard2009@yandex.ru>
2016-05-05 17:41:17 +0300

    Almost has finished offer panel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 (function(){
+
+	/*hide show serch panel*/
 	$(".navbar-right i").on("click", function(){
 		$("#search-form").slideToggle();
 	});
+
+	$("#slider span").on("click", changeImage)
+
+	function changeImage(e){
+		var target = $(e.target);
+		
+		if (target.attr("class").indexOf("left") == -1) {
+			var unVisibleImg = $(".visible").attr("class", "").next("img");
+
+			if(unVisibleImg.length){
+				unVisibleImg.fadeIn().addClass("visible");
+			}else{
+				$("#slider img").first().fadeIn().addClass("visible");
+			};
+			
+		}else{
+			var unVisibleImg = $(".visible").fadeOut().attr("class", "").prev("img");
+
+			if(unVisibleImg.length){
+				unVisibleImg.fadeIn().addClass("visible");
+			}else{
+				$("#slider img").fadeIn().last().addClass("visible");
+			};
+		};
+		console.log(unVisibleImg.length);
+	};
+
 }());
+
+/*$("#slider img").next("img");*/
